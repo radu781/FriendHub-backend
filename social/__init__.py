acfa0ea4e889c16f101ce1523d119f19254356da
radu781 <poparadu501@gmail.com>
2022-09-11 22:39:02 +0300

    Parity changes between local and server

--- social/__init__.py
+++ social/__init__.py
@@ -1,11 +1,11 @@
-import sys
-#sys.path.append("/var/www/social")
-#sys.path.append("/var/www/social/social")
-import os
-os.chdir("/var/www/social")
 from datetime import datetime
 from flask import Flask, render_template, request
 import config
+
+if config.DEPLOYING:
+    import os
+
+    os.chdir("/var/www/social")
 
 from controllers.deploy import deploy_blueprint
 
@@ -19,8 +19,8 @@ app.register_blueprint(deploy_blueprint)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", time=datetime.now(), ip=request.remote_addr, pwd=config.pwd)
+    return render_template("index.html", time=datetime.now(), ip=request.remote_addr)
 
 
 if __name__ == "__main__":
-    app.run(port=80, debug=False)
+    app.run(port=80, debug=config.DEBUG_ON)
--- social/config.py
+++ social/config.py
@@ -1,11 +1,10 @@
 from configparser import ConfigParser
-import os
 
-pwd = os.getcwd()
-print(pwd)
 ini_file = ConfigParser()
 ini_file.read("social/config/data.ini")
 
 SESSION_KEY = ini_file.get("pages", "key")
+DEBUG_ON = True if ini_file.get("pages", "debug") == "1" else False
+
 DEPLOY_KEY = ini_file.get("deploy", "key")
-DEBUG_ON = True if ini_file.get("pages", "debug") == 1 else False
+DEPLOYING = True if ini_file.get("deploy", "active") == "1" else False
